44e5bff43e7adad9d36fdf720df11d44d5280cad
Jonathan Kroening <jkroening@gmail.com>
2015-07-05 13:26:57 -0700

    Change Spotify lookup to use module.

--- Miscellaneous/popularity.py
+++ Miscellaneous/popularity.py
@@ -5,7 +5,11 @@
 import sys
 import json
 import fileinput
+import pdb
 import urllib2
+sys.path.append( "../Modules")
+from helpers import loadFile
+import spotify_methods as sp
 
 def spotifyLookup(track_id):
     track_string = 'http://ws.spotify.com/lookup/1/.json?uri=spotify:track:%s' % track_id.strip()
@@ -18,18 +22,32 @@ def spotifyLookup(track_id):
         return spotifyLookup(track_id)
 
 def main():
+    if len(sys.argv) > 1:
+        in_tracks = loadFile("", sys.argv[1])
+    else:
+        in_tracks = loadFile("input", "input.txt")
     track_list = []
-    for line in fileinput.input(sys.argv[1]):
+    local_tracks = []
+    for line in in_tracks:
+        if "local" in line:
+            local_tracks.append(line)
+            continue
         track_id = line.strip('http://open.spotify.com/track/')
-        track_data = spotifyLookup(track_id)
+        track_data = sp.pullSpotifyTrack(track_id)
         track_list.append(track_data)
         # track_list.append([str('spotify:track:%s' % track_id).strip(), track_id.strip(), '0.00'])
-    sorted_tracks = sorted(track_list, key=lambda x: float(x[2]), reverse=True)
+    pdb.set_trace()
+    sorted_tracks = sorted(track_list, key=lambda x: float(x['popularity']), reverse=True)
     # print sorted_tracks
+    print "\n"
     for item in sorted_tracks:
-        print item[1], item[2]
+        print str(item['popularity']) + " :: " + item['artist'] + " - " + item['title']
+    print "\n"
     for item in sorted_tracks:
-        print item[0]
+        print "spotify:track:{}".format(item['spotify_id'].strip())
+    for item in local_tracks:
+        print item
+    print "\n"
 
 if __name__ == "__main__":
     main()

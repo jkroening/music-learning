43e748cbfabd739fd455386f85e1ead698b0bc19
Jonathan Kroening <jonathan.kroening@ipsos.com>
2016-10-09 17:34:01 -0700

    Genre cluster into playlists script

--- Cluster Playlists/genre_cluster.py
+++ Cluster Playlists/genre_cluster.py
@@ -7,6 +7,10 @@
 import spotipy
 import fileinput
 import sys
+import pdb
+sys.path.append( "../Modules")
+import spotify_methods as sptfy
+
 
 def buildSubgenres():
     pop, urban, rock = [], [], []
@@ -18,28 +22,15 @@ def buildSubgenres():
         rock.append(line.strip())
     return pop, urban, rock
 
-def getArtist(track_id):
-    sp = spotipy.Spotify()
-    track = sp.track(track_id)
-    artist_id = track['artists'][0]['uri']
-    artist = track['artists'][0]['name']
-    return artist_id, artist
-
-def getGenres(artist_id):
-    sp = spotipy.Spotify()
-    artist = sp.artist(artist_id)
-    return artist['genres']
-
-def sortGenres(artist_id, pop, urban, rock, track_id):
+def sortGenres(artist_id, pop, urban, rock, track_id, token):
     p, u, r = 0, 0, 0
-    sp = spotipy.Spotify()
-    artist = sp.artist(artist_id)
-    if len(artist['genres']) < 1:
-        with open('output/unknowntracks.txt', 'a') as f:
+    genres = sptfy.getArtistGenres(artist_id, token = token)
+    if len(genres) < 1:
+        with open('../output/unknowntracks.txt', 'a') as f:
             f.write('spotify:track:%s\r' % track_id)
-            print artist
+            print "Not found: {}".format(track_id)
     else:
-        for g in artist['genres']:
+        for g in genres:
             if g in pop:
                 p = p + 1
             if g in urban:
@@ -48,46 +39,46 @@ def sortGenres(artist_id, pop, urban, rock, track_id):
                 r = r + 1
         # if there are ties, take the first genre listed of the ties
         if p == u and p > r:
-            if artist['genres'][0] in pop:
-                with open('output/poptracks.txt', 'a') as f:
+            if genres[0] in pop:
+                with open('../output/poptracks.txt', 'a') as f:
                     f.write('spotify:track:%s\r' % track_id)
-            elif artist['genres'][0] in urban:
-                with open('output/urbantracks.txt', 'a') as f:
+            elif genres[0] in urban:
+                with open('../output/urbantracks.txt', 'a') as f:
                     f.write('spotify:track:%s\r' % track_id)
         elif p == r and p > u:
-            if artist['genres'][0] in pop:
-                with open('output/poptracks.txt', 'a') as f:
+            if genres[0] in pop:
+                with open('../output/poptracks.txt', 'a') as f:
                     f.write('spotify:track:%s\r' % track_id)
-            elif artist['genres'][0] in rock:
-                with open('output/rocktracks.txt', 'a') as f:
+            elif genres[0] in rock:
+                with open('../output/rocktracks.txt', 'a') as f:
                     f.write('spotify:track:%s\r' % track_id)
         elif u == r and u > p:
-            if artist['genres'][0] in urban:
-                with open('output/urbantracks.txt', 'a') as f:
+            if genres[0] in urban:
+                with open('../output/urbantracks.txt', 'a') as f:
                     f.write('spotify:track:%s\r' % track_id)
-            elif artist['genres'][0] in rock:
-                with open('output/rocktracks.txt', 'a') as f:
+            elif genres[0] in rock:
+                with open('../output/rocktracks.txt', 'a') as f:
                     f.write('spotify:track:%s\r' % track_id)
         elif p == u and u == r:
-            if artist['genres'][0] in pop:
-                with open('output/poptracks.txt', 'a') as f:
+            if genres[0] in pop:
+                with open('../output/poptracks.txt', 'a') as f:
                     f.write('spotify:track:%s\r' % track_id)
-            elif artist['genres'][0] in urban:
-                with open('output/urbantracks.txt', 'a') as f:
+            elif genres[0] in urban:
+                with open('../output/urbantracks.txt', 'a') as f:
                     f.write('spotify:track:%s\r' % track_id)
-            elif artist['genres'][0] in rock:
-                with open('output/rocktracks.txt', 'a') as f:
+            elif genres[0] in rock:
+                with open('../output/rocktracks.txt', 'a') as f:
                     f.write('spotify:track:%s\r' % track_id)
         else: # there are no ties, so take the most frequent genre
             bigname = biggest(p, u, r)
             if bigname == 'p':
-                with open('output/poptracks.txt', 'a') as f:
+                with open('../output/poptracks.txt', 'a') as f:
                     f.write('spotify:track:%s\r' % track_id)
             elif bigname == 'u':
-                with open('output/urbantracks.txt', 'a') as f:
+                with open('../output/urbantracks.txt', 'a') as f:
                     f.write('spotify:track:%s\r' % track_id)
             elif bigname == 'r':
-                with open('output/rocktracks.txt', 'a') as f:
+                with open('../output/rocktracks.txt', 'a') as f:
                     f.write('spotify:track:%s\r' % track_id)
 
 def biggest(p, u, r):
@@ -103,19 +94,18 @@ def biggest(p, u, r):
 
 def main():
     pop, urban, rock = buildSubgenres()
-    genre_set = set()
-    for line in fileinput.input(sys.argv[1]):
+
+    if len(sys.argv) > 1:
+        in_file = sys.argv[1]
+    else:
+        in_file = "../input/input.txt"
+
+    token = sptfy.authSpotipy()
+
+    for line in fileinput.input(in_file):
         track_id = line.strip('http://open.spotify.com/track/').strip()
-        artist_id, artist = getArtist(track_id)
-        sortGenres(artist_id, pop, urban, rock, track_id)
-        # genres = getGenres(artist_id)
-        # print artist, genres
-        # for g in genres:
-        #     genre_set.add(g)
-    # sorted_genres = sorted(genre_set, key=lambda item: (int(item.partition(' ')[0]) if item[0].isdigit() else float('inf'), item))
-    # print sorted_genres
-    # for item in sorted_genres:
-    #     print item
+        artist_id = sptfy.pullSpotifyTrack(track_id)['spotify_artist_id']
+        sortGenres(artist_id, pop, urban, rock, track_id, token)
 
 if __name__ == "__main__":
     main()

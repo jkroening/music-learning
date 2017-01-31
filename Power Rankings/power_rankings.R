7694885fc49a9f7e51d34823c9d14f5c941d9697
Jonathan Kroening <jonathan.kroening@ipsos.com>
2017-01-30 21:21:30 -0800

    Various fixes to power_rankings.R

--- Power Rankings/power_rankings.R
+++ Power Rankings/power_rankings.R
@@ -8,6 +8,7 @@
 
 suppressMessages(library(spotifyr))
 suppressMessages(library(dplyr))
+library(stringi)
 
 args = commandArgs(trailingOnly = TRUE)
 
@@ -24,9 +25,12 @@ if (length(args) > 2)
 d.pre <- read.csv(args[1], stringsAsFactors = FALSE, encoding = "windows-1252",
                   sep = "\t")
 if (length(args) == 2) {
-    d.add <- read.csv(args[2], stringsAsFactors = FALSE, encoding = "iso-8859-1")
-    out <- system("file --mime new_albums.csv", intern = TRUE)
-    if (!grepl("iso-8859", out))
+    d.add <- suppressWarnings(
+        read.csv(args[2], stringsAsFactors = FALSE, encoding = "iso-8859-1")
+    )
+    out <- system(paste("file --mime", args[2]), intern = TRUE)
+    if (any(!stringi::stri_enc_isascii(unlist(d.add))) &&
+            !grepl("iso-8859", out) && !grepl("unknown-8bit", out))
         stop("You probably forgot to save ", args[2], " as a Windows Comma ",
              "Separated file!", call. = FALSE)
     d.add$ARTIST <- iconv(d.add$ARTIST, "iso-8859-1", "utf-8")
@@ -43,7 +47,7 @@ if (length(args) == 2) {
         ), stringsAsFactors = FALSE)
       , names(d.pre)
     )
-    if (args[2] == "new_eps.csv") {
+    if (grepl("new_eps.csv", args[2])) {
         out.name <- "EP_RANKINGS.csv"
         RELEASE.TYPE <- "EP"
     } else {
@@ -57,8 +61,8 @@ unlink("output/spotify_playlist.txt", force = TRUE)
 
 ## "static" vars
 LINK.PRE <- "<a href='"
-LINK.MID <- "' class='circle-thumbnail'><img src='http://afireintheattic.com/wp-content/uploads/"
-LINK.END <- ".png' alt='' width='50' height='50' class='center' /></a>"
+LINK.MID <- "' class='circle-thumbnail'><div class='circlecrop' style=background-image:url('http://afireintheattic.com/wp-content/uploads/"
+LINK.END <- ".jpg')></div></a>"
 trend.no <- "<div class='no_change'>--</span>"
 trend.up <- "<img src='http://afireintheattic.com/wp-content/uploads/up_gr_40.png' width='25' height='25' class='center' />"
 trend.dwn <- "<img src='http://afireintheattic.com/wp-content/uploads/down_rd_40.png' width='25' height='25' class='center' />"
@@ -112,12 +116,15 @@ d.sorted$POWER.INDEX <- as.numeric(d.sorted$POWER.INDEX)
 d1 <- d.pre[!is.na(d.pre$POWER.INDEX), ] ## previous index scores
 d2 <- as.numeric(d.sorted$POWER.INDEX) ## all index scores, including new
 
-cut1 <- cut(d1$POWER.INDEX,
-            quantile(d1$POWER.INDEX, probs = seq(0, 1, 0.1)),
-            labels = FALSE, include.lowest = TRUE)
-cut2 <- cut(d1$POWER.INDEX,
-            quantile(d2, probs = seq(0, 1, 0.1)),
-            labels = FALSE, include.lowest = TRUE)
+cuts <- if (length(d2) < 50) 0.25 else 0.2
+cuts <- if (length(d2) > 80) 0.10 else cuts
+
+cut1 <- .bincode(d1$POWER.INDEX,
+            quantile(d1$POWER.INDEX, probs = seq(0, 1, cuts)),
+            include.lowest = TRUE)
+cut2 <- .bincode(d1$POWER.INDEX,
+            quantile(d2, probs = seq(0, 1, cuts)),
+            include.lowest = TRUE)
 
 ## compare these two cuts takes albums previous in power ranking and looks at
 ## where they land in the decile compares them to where they land using the new
@@ -149,18 +156,79 @@ releaseSlug <- function(text) {
     text <- gsub("\\]", "", text)
     text <- gsub("\\'", "", text)
     text <- gsub("\\s", "", text)
+    text <- gsub("\\:", "", text)
     tolower(text)
 }
 
-get_track <- function(id){
+get_response_content <- function(response) {
+    print("-------- NEW ONE --------")
+    print(response)
+    print(content(response))
+    print(status_code(response))
+    print(str(response))
+    if (status_code(response) == 429) browser()
+    if(!(status_code(response) %in% c(200,201,204))) {
+        stop(paste(
+            '\nError Code: '
+          , content(response)$error$status
+          , '\n'
+          , content(response)$error$message
+        ))
+    }
+    # Otherwise, return content
+    content(response)
+}
+assignInNamespace("get_response_content", get_response_content, ns = "spotifyr")
+
+get_track <- function(id) {
     tracks_url <- paste0(spotifyr::base_url, '/v1/tracks/')
     search <- GET(url = paste0(tracks_url, id))
     get_response_content(search)
 }
 
+search <- function(q, type, ...) {
+    search_url <- paste(base_url,'/v1/search',sep='')
+    response <- GET(url = search_url,
+                    query=list(q=q,type=type,...),
+                    add_headers(Authorization=paste('Bearer',SPOTIFY_ACCESS_TOKEN)))
+    get_response_content(response)
+}
+assignInNamespace("search", search, ns = "spotifyr")
+
+get_artist_toptracks <- function(id, country, ...) {
+  search <- GET(url = paste(artists_url,id,'/top-tracks',sep=''),
+                query=list(country=country),
+                add_headers(Authorization=paste('Bearer',SPOTIFY_ACCESS_TOKEN)))
+  get_response_content(search)
+}
+assignInNamespace("get_artist_toptracks", get_artist_toptracks, ns = "spotifyr")
+
+get_album_tracks <- function(id, ...) {
+  search <- GET(url = paste(albums_url,id,'/','tracks',sep=''),query=list(...),
+                add_headers(Authorization=paste('Bearer',SPOTIFY_ACCESS_TOKEN)))
+  get_response_content(search)
+}
+assignInNamespace("get_album_tracks", get_album_tracks, ns = "spotifyr")
+
+get_artist_albums <- function(id, ...) {
+  search <- GET(url = paste(artists_url,id,'/albums',sep=''),
+                query=list(...),
+                add_headers(Authorization=paste('Bearer',SPOTIFY_ACCESS_TOKEN)))
+  get_response_content(search)
+}
+assignInNamespace("get_artist_albums", get_artist_albums, ns = "spotifyr")
+
+getter <- function(url, query, ...) {
+    response <- GET(url = url, query = query, ...)
+    if (status_code(response) == 429) {
+        print("Too many requests. Waiting...")
+
+    }
+}
+
 manualURI <- function(artist, release) {
     cat("What is the Spotify URI of the most popular song on the release? ")
-    cat("(Enter song title if not available on Spotify): ")
+    cat("(Enter song title or local link if not available on Spotify): ")
     track <- readLines(con = "stdin", 1)
     if (!grepl("spotify:track:", track) && !grepl("open.spotify.com", track))
         track <- paste0(
@@ -174,6 +242,7 @@ manualURI <- function(artist, release) {
 
 manualURL <- function() {
     cat("What is the URL of the release? ")
+    cat("(Enter URL to link from image if not available on Spotify): ")
     release.url <- readLines(con = "stdin", 1)
     warning("You will need to download the release artwork manually.",
             call. = FALSE, immediate. = TRUE)
@@ -217,7 +286,11 @@ parseRankings <- function(i, df) {
     artist <- df$ARTIST[[i]]
     cat("\n")
     print(decodeString(artist, toLower = FALSE))
-    artists <- spotifyr::search(artist, type = 'artist')$artists$items
+    artists <- NULL
+    while (is.null(artists)) {
+        artists <- spotifyr::search(artist, type = 'artist')$artists$items
+        if (is.null(artists)) Sys.sleep(5)
+    }
     if (!nchar(df$TREND[[i]])) {
         return(findEntry(artists, df[i, ]))
     } else {
@@ -247,6 +320,7 @@ findEntry <- function(artists, entry, firstAttempt = TRUE, topTrackOnly = FALSE,
             artists
           , entry
           , firstAttempt = FALSE
+          , topTrackOnly = topTrackOnly
         ))
     } else if (is.null(artist.id) && artistIter == 1) {
         warning("Artist '", artist, "' not found. You will need to complete ",
@@ -298,10 +372,11 @@ findEntry <- function(artists, entry, firstAttempt = TRUE, topTrackOnly = FALSE,
                         next
                     release.url <- releases[[k]]$external_urls
                     dir.create(file.path("./imgs"), showWarnings = FALSE)
-                    download.file(releases[[k]]$images[[1]]$url,
-                                  file.path("./imgs/",
-                                            paste0(release.trunc, ".png")),
-                                  quiet = TRUE)
+                    if (!topTrackOnly)
+                           download.file(releases[[k]]$images[[1]]$url,
+                                         file.path("./imgs/",
+                                                   paste0(release.trunc, ".png")),
+                                         quiet = TRUE)
                     tracks.top <- spotifyr::simplify_result(
                         spotifyr::get_artist_toptracks(artist.id, country = "US")
                       , type = "songs"
@@ -340,6 +415,19 @@ findEntry <- function(artists, entry, firstAttempt = TRUE, topTrackOnly = FALSE,
     return(findEntry(artists, entry, firstAttempt, topTrackOnly, artistIter = artistIter + 1))
 }
 
+## authorize spotifyr
+config <- read.csv("../config/config.csv", stringsAsFactors = FALSE, header = FALSE)
+SPOTIFY_CLIENT_ID = config[config[[1]] == "SPOTIFY_CLIENT_ID", 2]
+SPOTIFY_CLIENT_SECRET = config[config[[1]] == "SPOTIFY_CLIENT_SECRET", 2]
+SPOTIFY_REDIRECT_URI = config[config[[1]] == "SPOTIFY_REDIRECT_URI", 2]
+spotifyr::set_credentials(
+    client_id = SPOTIFY_CLIENT_ID
+  , client_secret = SPOTIFY_CLIENT_SECRET
+  , client_redirect_uri = SPOTIFY_REDIRECT_URI
+)
+SPOTIFY_ACCESS_TOKEN <- spotifyr::get_tokens()$access_token
+
+unlink("./imgs", recursive = TRUE, force = TRUE)
 d.out <- data.frame(
     do.call(rbind, lapply(1:nrow(d.sorted), parseRankings, d.sorted))
   , stringsAsFactors = FALSE

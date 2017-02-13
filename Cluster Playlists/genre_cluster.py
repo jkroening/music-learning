cb4f7f7b879a2c77da58f1a6f19104eaf0842c7b
Jonathan Kroening <jonathan.kroening@ipsos.com>
2017-02-12 22:07:56 -0800

    Genre clustering script complete.

--- Cluster Playlists/genre_cluster.py
+++ Cluster Playlists/genre_cluster.py
@@ -8,15 +8,15 @@ import spotipy
 import fileinput
 import sys
 import os
-import pdb
+import glob
 sys.path.append( "../Modules")
 import spotify_methods as sptfy
 import db_methods as dbm
-import helpers and hlpr
+import helpers as hlpr
+import module_helpers as mhlpr
 
 
 def main():
-    genres = dbm.buildSubgenres()
 
     if len(sys.argv) > 1:
         in_file = sys.argv[1]
@@ -25,12 +25,18 @@ def main():
 
     token = sptfy.authSpotipy()
 
-    os.remove("../output/*.txt")
+    for f in glob.glob("../output/*.txt"):
+        os.remove(f)
 
     for line in fileinput.input(in_file):
-        track_id = line.strip('http://open.spotify.com/track/').strip()
-        artist_id = sptfy.pullSpotifyTrack(track_id)['spotify_artist_id']
-        hlpr.sortGenres(artist_id, genres, track_id, token, makePlaylists = True)
+        track_id = line.split("/")[-1].strip()
+        track_info = sptfy.pullSpotifyTrack(track_id)
+        artist_id = track_info['spotify_artist_id']
+        artist_name = track_info['artist']
+        track_name = track_info['title']
+        secondary_artist = track_info['secondary_artist']
+        hlpr.sortGenres(artist_name, artist_id, track_name, track_id,
+                        secondary_artist, token, makePlaylists = True)
 
 if __name__ == "__main__":
     main()

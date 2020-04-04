d43cc51e577c0367e8f333f7a521fa361f76c91f
Jonathan Kroening <jonathan.kroening@ipsos.com>
2020-04-04 14:51:01 -0700

    No more ultimatechart.

--- Airplay/spotifyairplay.py
+++ Airplay/spotifyairplay.py
@@ -11,11 +11,6 @@ from helpers import loadFile
 
 def main(ags, explicit = False):
 
-    if args.ultimatechart is not None:
-        with open(args.ultimatechart, 'r') as f:
-            ultimatechart = f.readlines()
-    else:
-        ultimatechart = None
     if args.billboardchart is not None:
         with open(args.billboardchart, 'r') as f:
             billboardchart = [line.rstrip() for line in f]
@@ -56,37 +51,8 @@ def main(ags, explicit = False):
     for item in sorted_tracks:
         print("spotify:track:{}".format(item['spotify_id'].strip()))
     for item in local_tracks:
-        print item
-    print "\n"
-
-    if ultimatechart is not None or billboardchart is not None:
-        print "Look into keeping the following songs in Airplay this week...\n"
-
-    if ultimatechart is not None:
-        nums = ('01', '02', '03', '04', '05', '06', '07', '08', '09') + tuple(str(x) for x in range(10, 101))
-        uc = [item for item in ultimatechart if item.startswith(nums)]
-        for item in sorted_tracks:
-            if item['popularity'] < 75:
-                artist = item['artist']
-                title = item['title']
-                artist = re.sub("&", "and", re.sub(r'([^\s\w]|_)+', '', artist).lower())
-                title = re.sub("&", "and", re.sub(r'([^\s\w]|_)+', '', title).lower())
-                for u in uc:
-                    x = re.sub(r'([^\s\w]|_)+', '', re.sub("&", "and", u)).lower()
-                    if artist + " " in x:
-                        artist_match = True
-                    else:
-                        artist_match = False
-                    if title + " by" in x:
-                        title_match = True
-                    else:
-                        title_match = False
-                    xs = [i for i in x.split(" ")]
-                    if title_match or (artist_match and any([True for t in title.split(" ") if t in xs])):
-                        print str(item['popularity']) + " :: " + item['artist'] + " - " + item['title']
-                        break
-    else:
-        print "\nIf you want to compare to Ultimate Chart, please provide a txt version as an arg to --ultimatechart"
+        print(item)
+        print("\n")
 
     if billboardchart is not None:
         print("\nLook into keeping the following songs in Airplay this week...\n")
@@ -118,7 +84,6 @@ def main(ags, explicit = False):
 if __name__ == "__main__":
     explicit = False
     parser = argparse.ArgumentParser()
-    parser.add_argument('--ultimatechart')
     parser.add_argument('--billboardchart')
     parser.add_argument('--explicit')
     args = parser.parse_args()

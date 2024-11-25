7015eeb0600dd810fd3a8712e345f9d23269876d
Jonathan Kroening <jonathan.kroening@ipsos.com>
2024-11-24 20:04:23 -0800

    Proper comment style.

--- DRM Removal/drmstrip.py
+++ DRM Removal/drmstrip.py
@@ -5,8 +5,8 @@ for fn in os.listdir('in'):
     if fn.endswith(".m4a") or fn.endswith(".mp3") or fn.endswith(".m4p"):
 
         inputfile = "in/%s" % fn
-        # if fn.endswith(".m4p"):
-        #     fn = fn.replace(".m4p", ".m4a")
+        ## if fn.endswith(".m4p"):
+        ##     fn = fn.replace(".m4p", ".m4a")
         outputfile = "out/%s" % fn
 
         cmd = ['ffmpeg', '-i', inputfile, '-vn', '-acodec', 'copy', '-metadata', 'copyright=', outputfile]
